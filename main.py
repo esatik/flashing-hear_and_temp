d3709b630d5978c212956079d0868be800852c21
Eren SATIK <49689257+esatik@users.noreply.github.com>
2025-04-09 20:25:25 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,10 @@
+def on_forever():
+    basic.show_icon(IconNames.HEART)
+    basic.clear_screen()
+    basic.pause(500)
+    basic.show_icon(IconNames.SMALL_HEART)
+    basic.clear_screen()
+    basic.pause(500)
+    basic.show_string(str(input.temperature()))
+    basic.pause(500)
+basic.forever(on_forever)
